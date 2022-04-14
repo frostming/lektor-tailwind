0e290fc3700f8de3b144f283a86f0214eb216645
Frost Ming <mianghong@gmail.com>
2022-04-14 13:51:07 +0800

    add CI script

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ ENTRY_POINTS = {
 
 setup_kwargs = {
     "name": "lektor-tailwind",
-    "version": "0.1",
+    "version": "0.1.dev4+g40abe28.d20220414",
     "description": "",
     "long_description": long_description,
     "license": "MIT",
@@ -23,6 +23,14 @@ setup_kwargs = {
     "maintainer_email": None,
     "url": "",
     "package_data": {"": ["*"]},
+    "classifiers": [
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
     "install_requires": INSTALL_REQUIRES,
     "python_requires": ">=3.8",
     "entry_points": ENTRY_POINTS,
